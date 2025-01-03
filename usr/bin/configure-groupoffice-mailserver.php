8b27ea3156a87481dcac59fba7e93e33852d08cd
Merijn Schering <mschering@intermesh.nl>
2025-01-03 12:56:35 +0100

    maildomains

--- usr/bin/configure-groupoffice-mailserver.php
+++ usr/bin/configure-groupoffice-mailserver.php
@@ -1,19 +1,35 @@
 #!/usr/bin/php
 <?php
 require ("/etc/groupoffice/config.php");
+/* @var $config array */
 
 echo "Applying group-office database credentials to /etc/dovecot/dovecot-sql.conf.ext and /etc/postfix/mysql_*\n";
 
-$dovecotConnectStr = 'connect = "host='.$config['db_host'].' dbname='.$config['db_name'].' user='.$config['db_user'].' password='.$config['db_pass'].'"';
 
 // will escape dollar followed by digest
 function escape_backreference($x){
     return preg_replace('/\$(\d)/', '\\\$$1', $x);
 }
 
-$data = file_get_contents('/etc/dovecot/dovecot-groupoffice-sql.conf.ext');
-$data = preg_replace('/connect = ".*"/', escape_backreference($dovecotConnectStr), $data);
-file_put_contents('/etc/dovecot/dovecot-groupoffice-sql.conf.ext', $data);
+
+function replaceDovecotCred($config, $file) {
+	$dovecotConnectStr = 'connect = "host='.$config['db_host'].' dbname='.$config['db_name'].' user='.$config['db_user'].' password='.$config['db_pass'].'"';
+
+	$data = file_get_contents($file);
+	$data = preg_replace('/connect = ".*"/', escape_backreference($dovecotConnectStr), $data);
+	file_put_contents($file, $data);
+}
+
+replaceDovecotCred($config,'/etc/dovecot/dovecot-groupoffice-sql.conf.ext');
+replaceDovecotCred($config,'/etc/dovecot/domain-owner-sql.conf.ext');
+replaceDovecotCred($config,'/etc/dovecot/dovecot-dict-sql.conf.ext');
+
+
+$data = file_get_contents('/etc/opendkim.conf');
+$data = preg_replace('/SigningTable.*/', 'SigningTable dsn:mysql://'.$config['db_user'].':'.escape_backreference($config['db_pass']).'@'.$config['db_host'].'/'.$config['db_name'].'/table=community_maildomains_dkim?keycol=domain?datacol=id', $data);
+$data = preg_replace('/KeyTable.*/', 'KeyTable dsn:mysql://'.$config['db_user'].':'.escape_backreference($config['db_pass']).'@'.$config['db_host'].'/'.$config['db_name'].'/table=community_maildomains_dkim?keycol=id?datacol=domain,selector,privateKey', $data);
+file_put_contents('/etc/opendkim.conf', $data);
+
 
 function replacePostfixCred($config, $file) {
 	
