32aa906e18c8c7823433331466262c540e70164b
Merijn Schering <mschering@intermesh.nl>
2025-01-13 14:06:13 +0100

    25.0.1

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 docker login
-docker buildx build --push --platform linux/amd64,linux/arm64 --no-cache . -t intermesh/groupoffice-mailserver:latest
+docker buildx build --push --platform linux/amd64,linux/arm64 --no-cache . -t intermesh/groupoffice-mailserver:latest -t testing
 
 #docker push intermesh/groupoffice-mailserver:latest
